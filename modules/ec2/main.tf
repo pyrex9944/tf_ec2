6d2381cc61b102d4a62579f4f803f9737574982d
ashirov <iprochechya@gmail.com>
2023-01-15 21:42:56 +0500

    init

---
+++ modules/ec2/main.tf
@@ -0,0 +1,52 @@
+
+
+// Create aws_ami filter to pick up the ami available in your region
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+// Configure the EC2 instance in a public subnet
+resource "aws_instance" "ec2_public" {
+  ami                         = data.aws_ami.amazon-linux-2.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = var.vpc.public_subnets[0]
+  vpc_security_group_ids      = [var.sg_pub_id]
+
+  tags = {
+    "Name" = "${var.namespace}ec2"
+  }
+
+  # Copies the ssh key file to home dir
+  provisioner "file" {
+    source      = "./${var.key_name}.pem"
+    destination = "/home/ubuntu/${var.key_name}.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+  }
+  
+  //chmod key 400 on EC2 instance
+  provisioner "remote-exec" {
+    inline = ["chmod 400 ~/${var.key_name}.pem"]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "public_connection_string" {
+  description = "Copy/Paste/Enter - You are in the matrix"
+  value       = "ssh -i ${module.ssh-key.key_name}.pem ec2-user@${module.ec2.public_ip}"
+}
